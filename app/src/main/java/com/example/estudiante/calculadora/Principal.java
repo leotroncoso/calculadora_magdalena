5f89846b3244b15162f22aeebc710b4a65b95ec5
Roberto <roberto@morales.com.co>
2019-07-06 11:38:23 -0500

    Suma dos numeros pero no borra

--- app/src/main/java/com/example/estudiante/calculadora/Principal.java
+++ app/src/main/java/com/example/estudiante/calculadora/Principal.java
@@ -2,12 +2,28 @@ package com.example.estudiante.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
-
+    TextView resultado;
+    EditText num1,num2;
+    double n1, n2, suma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        num1 = findViewById(R.id.txtNumeroUno);
+        num2 = findViewById(R.id.txtNumeroDos);
+        resultado = findViewById(R.id.txtResultado);
+
+    }
+
+    public void calcular(View v){
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        suma = n1 + n2;
+        resultado.setText(""+suma);
     }
 }
